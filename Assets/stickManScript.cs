f8b14aeb95eedd8d1a69a0cc5d4c221019da82a0
Ampherost <Ampherost235@gmail.com>
2024-10-05 23:13:10 -0700

    grounded jumps pt1

--- Assets/stickManScript.cs
+++ Assets/stickManScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class stickManScript : MonoBehaviour
 {
     public Rigidbody2D stickRigidBody;
+    public GameObject stage;
 
     // Start is called before the first frame update
     void Start()
