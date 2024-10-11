5b71bcacfe27e2d141161368c2db6ae239258d30
Ampherost <Ampherost235@gmail.com>
2024-10-11 16:50:08 -0700

    added ui to track player 2 hp

---
+++ Assets/enemyHealthScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class enemyHealthScript : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI healthText;
+    public enemyScript enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = "Player 2 HP: " + enemy.health.ToString();
+    }
+}
