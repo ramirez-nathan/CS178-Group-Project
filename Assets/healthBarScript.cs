c63d146eb930a2b3d77767ebca95599b069a2f30
Ampherost <Ampherost235@gmail.com>
2024-10-15 21:33:36 -0700

    working health bar

--- Assets/healthBarScript.cs
+++ Assets/healthBarScript.cs
@@ -1,12 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class healthBarScript : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI healthText;
-    public PlayerScript stickMan;
+    public PlayerScript player;       // reference to our player
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +15,24 @@ public class healthBarScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        healthText.text = "Player 1 HP: " + stickMan.currentHealth.ToString();
+        // Check if the player reference is still valid and that maxHealth is not zero
+        if (player != null && player.health > 0)
+        {
+            // Calculate the health percentage (between 0 and 1)
+            float healthPercentage = player.currentHealth / player.health;
+
+            // Ensure healthPercentage is between 0 and 1
+            healthPercentage = Mathf.Clamp(healthPercentage, 0, 1);
+
+            // Update the scale of the health bar based on the health percentage
+            Vector3 scale = transform.localScale;
+            scale.x = healthPercentage; // Adjust only the x scale to change the width of the health bar
+            transform.localScale = scale;
+        }
+        else
+        {
+            // Optionally disable or destroy the health bar when the player is destroyed
+            gameObject.SetActive(false); // Disables the health bar when the player is null or maxHealth is 0
+        }
     }
 }
---
+++ Assets/healthTextScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class healthTextScript : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI healthText;
+    public PlayerScript stickMan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = "Player 1 HP: " + stickMan.currentHealth.ToString();
+    }
+}
