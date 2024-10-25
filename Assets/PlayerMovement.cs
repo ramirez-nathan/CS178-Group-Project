5d35a055946df8b995d32160dca6cd50433716fc
ramirez-nathan <nater0826@gmail.com>
2024-10-25 10:38:42 -0700

    Created playermovement class, added serialized move speed and current velocity vector

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using PlayerStates;
+using UnityEngine.EventSystems;
+using UnityEngine.Rendering;
+using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+    public float moveSpeed = 10f;
+    public Vector2 currentVelocity = Vector2.zero;
     // Start is called before the first frame update
     void Start()
     {
