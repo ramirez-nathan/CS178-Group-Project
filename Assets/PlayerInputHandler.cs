5b6a2e2d469cc28298ff5b4a1678b10792ea1820
ramirez-nathan <nater0826@gmail.com>
2024-10-25 10:36:00 -0700

    Created a struct of playerinputactions

--- Assets/PlayerInputHandler.cs
+++ Assets/PlayerInputHandler.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerInputHandler : MonoBehaviour
 {
+    public struct PlayerActions
+    { 
+        public InputAction move; // joystick/WASD
+        public InputAction jump; // Space/South Button
+        public InputAction neutralGAttack; // neutral ground L/East
+        public InputAction dashGAttack; // moving ground L/East
+    }
+    PlayerActions playerControls;
     // Start is called before the first frame update
     void Start()
     {
