bfb39a70920e75203fc47ea4e88b8d492d69b1ed
ramirez-nathan <nater0826@gmail.com>
2024-10-25 10:36:53 -0700

    Added player movement and playerinput references. Implemented Awake() that also indexes the playermovement files to find the correct player

--- Assets/PlayerInputHandler.cs
+++ Assets/PlayerInputHandler.cs
@@ -16,6 +16,22 @@ public class PlayerInputHandler : MonoBehaviour
         public InputAction dashGAttack; // moving ground L/East
     }
     PlayerActions playerControls;
+
+    private PlayerMovement playerMovement;
+    private PlayerInput playerInput; 
+    
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        // Finds all objects with playermovement component attached
+        var playerMovements = FindObjectsOfType<PlayerMovement>();
+
+        // retrieves player index 
+        var index = playerInput.playerIndex;
+
+        // Finds the PlayerMovement with the matching player index to associate it with this player
+        playerMovement = playerMovements.FirstOrDefault(m => m.GetPlayerIndex() == index);
+    }
     // Start is called before the first frame update
     void Start()
     {
