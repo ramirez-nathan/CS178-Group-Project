a4934a4d92d3d2956629ea656c4579b5d92d91ef
Ampherost <Ampherost235@gmail.com>
2024-10-29 13:22:12 -0700

    added gameover screen

--- Assets/PlayerMain.cs
+++ Assets/PlayerMain.cs
@@ -12,6 +12,7 @@ public class PlayerMain : MonoBehaviour
     public AudioSource deathSound;       // A sound that gets played when the character gets destroyed
     public Collider2D attackCollider;    // The collider representing the player's attack hitbox
     //public enemyScript enemyScwipt;      // Reference to enemy code
+    public gameOverScreen gameOverScween; // The game over screen
 
     // Sprites
     public Sprite attack;                // Sprite for the attack action.
