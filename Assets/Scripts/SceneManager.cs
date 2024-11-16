e5e43559ab331bc470885588975f15ab9d723afb
Ampherost <Ampherost235@gmail.com>
2024-11-15 23:54:53 -0800

    removed redundancy

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -7,9 +7,6 @@ using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    public Button selectModeButton;
-    public Button startGameButton;
-
     // Start is called before the first frame update
     void Start()
     {
